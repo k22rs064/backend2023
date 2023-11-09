07e31f8f3fff51939f85a607b3295a607e73c970
k22rs064 <k22rs064@st.kyusan-u.ac.jp>
2023-11-09 10:28:07 +0900

    backend2023作成

---
+++ www/key.js
@@ -0,0 +1,4 @@
+// This is a JavaScript file
+
+let appKey = "7b657a8ce118a69764ece5fc2f3bec15ace4ccc727d229675885254d092e841e";
+let clientKey = "b84a85d43f68bae8cd20ca8861c2e67dc31590e0f52ce1d6e6508c4f0ddc0c11";
